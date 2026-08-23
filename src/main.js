290fe77f20671e8f41452b8b0fd3740d8ecd74bf
Mark Gil <markgil.p.sendin@gmail.com>
2026-08-23 12:57:41 +0800

    feature: Setup google analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import VueGtag from 'vue-gtag-next'
 
 const app = createApp(App)
 
 app.use(router)
+
+const gaId = import.meta.env.VITE_GA_MEASUREMENT_ID
+
+if (gaId) {
+  app.use(VueGtag, {
+    property: {
+      id: gaId
+    }
+  }, router)
+}
+
 app.mount('#app')
